f3ffbe2722fdf7582ff5e0a3d4f4978311f561b4
piyush <piyushmakhija5@gmail.com>
2014-03-02 11:52:27 +0530

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,11 +9,17 @@ describe "Static pages" do
       	:text => 'Sample App')
     end
 
-  	it "should have the title 'Home' " do
+  	it "should have the base title" do
   		visit '/static_pages/home'
   		page.should have_selector('title', :text => 
-  			"Home")
+  			"Ruby on Rails Tutorial Sample App")
   	end
+
+    it "should not have a custom page title " do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => 
+        '| Home')
+    end
   end
 
   describe "Help page" do
@@ -42,5 +48,19 @@ describe "Static pages" do
   		page.should have_selector('title', 
   			:text => "About Us")
   	end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact' " do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', 
+        :text => 'Contact')
+    end
+
+    it "should have the title 'Contact' " do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+        :text => "Contact")
+    end
   end
 end
